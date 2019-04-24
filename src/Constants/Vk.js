9281a2e975f0f677519989dc4d9a3d6dceb47c2c
Svetlana <sbatova@bitia.ru>
2019-04-24 13:55:55 +0300

    GKN-60: update vk constants

--- src/Constants/Vk.js
+++ src/Constants/Vk.js
@@ -1,2 +1,5 @@
-export const CLIENT_ID = 6454126;
-export const REDIRECT_URI = 'http://demo1.bitia.ru/api/v1/integrations/vk/callbacks/oauth2';
+import ApiUrl   from '../ApiUrl';
+import ClientId from '../ClientId';
+
+export const CLIENT_ID = ClientId;
+export const REDIRECT_URI = `${ApiUrl}/v1/integrations/vk/callbacks/oauth2`;
