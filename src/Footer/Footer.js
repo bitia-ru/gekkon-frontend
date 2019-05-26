1732f7265fb3db848ccafc9e34a9c1f65d1f196b
ALev <alev@bitia.ru>
2019-05-26 06:37:02 +0300

    GKN-90: Исправление замечаний по coding styles

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,104 +1,135 @@
-import React, {Component} from 'react';
-import PropTypes          from 'prop-types';
-import {Link}             from 'react-router-dom';
-import SocialLinkButton   from '../SocialLinkButton/SocialLinkButton';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import SocialLinkButton from '../SocialLinkButton/SocialLinkButton';
 import {
-    VK_LINK,
-    FACEBOOK_LINK,
-    TWITTER_LINK,
+  VK_LINK,
+  FACEBOOK_LINK,
+  TWITTER_LINK,
 } from '../Constants/SocialLinks';
 import './Footer.css';
 
-export default class Footer extends Component {
-    render() {
-        return <footer className="footer">
-            <div className="footer__container">
-                <div className="footer__block-group">
-                    <div className="footer__item">
-                        <h3 className="footer__header">
-                            Аккаунт
-                        </h3>
-                        <ul className="footer__list">
-                            {this.props.user === null ? <React.Fragment>
-                                <li className="footer__list-item">
-                                    <a onClick={this.props.logIn} className="footer__list-link">
-                                        Вход
-                                    </a>
-                                </li>
-                                <li className="footer__list-item">
-                                    <a onClick={this.props.signUp} className="footer__list-link">
-                                        Регистрация
-                                    </a>
-                                </li>
-                            </React.Fragment> : <li className="footer__list-item">
-                                <a onClick={this.props.logOut} className="footer__list-link">
-                                    Выход
-                                </a>
-                            </li>}
-                        </ul>
-                    </div>
-                    <div className="footer__item">
-                        <h3 className="footer__header">
-                            Разделы
-                        </h3>
-                        <ul className="footer__list">
-                            <li className="footer__list-item">
-                                <Link to="/" className="footer__list-link">Скалодромы</Link>
-                            </li>
-                            <li className="footer__list-item">
-                                <Link to="/crags" className="footer__list-link">Скалы</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer__item">
-                        <h3 className="footer__header">
-                            Информация
-                        </h3>
-                        <ul className="footer__list">
-                            <li className="footer__list-item">
-                                <Link to="/about" className="footer__list-link">О нас</Link>
-                            </li>
-                            <li className="footer__list-item">
-                                <Link to="/howtohelp" className="footer__list-link">Как нам помочь</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer__item">
-                        <h3 className="footer__header">
-                            Помощь
-                        </h3>
-                        <ul className="footer__list">
-                            <li className="footer__list-item">
-                                <Link to="/faq" className="footer__list-link">FAQ</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="footer__social">
-                    <h3 className="footer__header">
-                        Соцсети
-                    </h3>
-                    <ul className="social-links">
-                        <li><SocialLinkButton href={VK_LINK}
-                                              xlinkHref="/public/img/social-links-sprite/social-links-sprite.svg#icon-vk"/>
-                        </li>
-                        <li><SocialLinkButton href={FACEBOOK_LINK}
-                                              xlinkHref="/public/img/social-links-sprite/social-links-sprite.svg#icon-facebook"/>
-                        </li>
-                        <li>
-                            <SocialLinkButton href={TWITTER_LINK}
-                                              xlinkHref="/public/img/social-links-sprite/social-links-sprite.svg#icon-twitter">
-                            </SocialLinkButton>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </footer>;
-    }
-}
+const Footer = ({
+  logIn, signUp, logOut, user,
+}) => (
+  <footer className="footer">
+    <div className="footer__container">
+      <div className="footer__block-group">
+        <div className="footer__item">
+          <h3 className="footer__header">
+            Аккаунт
+          </h3>
+          <ul className="footer__list">
+            {
+              user === null
+                ? (
+                  <React.Fragment>
+                    <li className="footer__list-item">
+                      <a
+                        onClick={logIn}
+                        role="link"
+                        tabIndex={0}
+                        className="footer__list-link"
+                      >
+                        Вход
+                      </a>
+                    </li>
+                    <li className="footer__list-item">
+                      <a
+                        onClick={signUp}
+                        role="link"
+                        tabIndex={0}
+                        className="footer__list-link"
+                      >
+                        Регистрация
+                      </a>
+                    </li>
+                  </React.Fragment>
+                )
+                : (
+                  <li className="footer__list-item">
+                    <a
+                      onClick={logOut}
+                      role="link"
+                      tabIndex={0}
+                      className="footer__list-link"
+                    >
+                      Выход
+                    </a>
+                  </li>
+                )
+            }
+          </ul>
+        </div>
+        <div className="footer__item">
+          <h3 className="footer__header">
+            Разделы
+          </h3>
+          <ul className="footer__list">
+            <li className="footer__list-item">
+              <Link to="/" className="footer__list-link">Скалодромы</Link>
+            </li>
+            <li className="footer__list-item">
+              <Link to="/crags" className="footer__list-link">Скалы</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__item">
+          <h3 className="footer__header">Информация</h3>
+          <ul className="footer__list">
+            <li className="footer__list-item">
+              <Link to="/about" className="footer__list-link">О нас</Link>
+            </li>
+            <li className="footer__list-item">
+              <Link to="/howtohelp" className="footer__list-link">Как нам помочь</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__item">
+          <h3 className="footer__header">Помощь</h3>
+          <ul className="footer__list">
+            <li className="footer__list-item">
+              <Link to="/faq" className="footer__list-link">FAQ</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer__social">
+        <h3 className="footer__header">Соцсети</h3>
+        <ul className="social-links">
+          <li>
+            <SocialLinkButton
+              href={VK_LINK}
+              xlinkHref="/public/img/social-links-sprite/social-links-sprite.svg#icon-vk"
+            />
+          </li>
+          <li>
+            <SocialLinkButton
+              href={FACEBOOK_LINK}
+              xlinkHref="/public/img/social-links-sprite/social-links-sprite.svg#icon-facebook"
+            />
+          </li>
+          <li>
+            <SocialLinkButton
+              href={TWITTER_LINK}
+              xlinkHref="/public/img/social-links-sprite/social-links-sprite.svg#icon-twitter"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+);
 
 Footer.propTypes = {
-    logIn: PropTypes.func.isRequired,
-    signUp: PropTypes.func.isRequired,
-    logOut: PropTypes.func.isRequired
+  user: PropTypes.object,
+  logIn: PropTypes.func.isRequired,
+  signUp: PropTypes.func.isRequired,
+  logOut: PropTypes.func.isRequired,
 };
+
+Footer.defaultProps = {
+  user: null,
+};
+
+export default Footer;
--- src/SocialLinkButton/SocialLinkButton.js
+++ src/SocialLinkButton/SocialLinkButton.js
@@ -1,17 +1,49 @@
 import React, {Component} from 'react';
-import PropTypes          from 'prop-types';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './SocialLinkButton.css';
 
 export default class SocialLinkButton extends Component {
-    render() {
-        return <React.Fragment><a href={this.props.href ? this.props.href : null} onClick={this.props.onClick ? this.props.onClick : null} className={'social-links__link' + (this.props.dark ? ' social-links__link_dark' : '') + (this.props.active ? ' social-links__link_active' : '') + (this.props.withRemoveButton ? ' social-link__with_remove' : '') + (this.props.unactive ? ' social-link__with_remove_unactive' : '')}>
-            <svg>
-                <use xlinkHref={this.props.xlinkHref}></use>
-            </svg>
-        </a></React.Fragment>;
-    }
+  render() {
+    const aClassNames = classNames({
+      'social-links__link': true,
+      'social-links__link_dark': this.props.dark,
+      'social-links__link_active': this.props.active,
+      'social-link__with_remove': this.props.withRemoveButton,
+      'social-link__with_remove_unactive': this.props.unactive,
+    });
+
+    return (
+      <React.Fragment>
+        <a
+          href={this.props.href}
+          onClick={this.props.onClick}
+          className={aClassNames}
+        >
+          <svg>
+            <use xlinkHref={this.props.xlinkHref} />
+          </svg>
+        </a>
+      </React.Fragment>
+    );
+  }
 }
 
 SocialLinkButton.propTypes = {
-    xlinkHref: PropTypes.string.isRequired
+  href: PropTypes.string,
+  dark: PropTypes.bool,
+  active: PropTypes.bool,
+  unactive: PropTypes.bool,
+  onClick: PropTypes.func,
+  withRemoveButton: PropTypes.bool,
+  xlinkHref: PropTypes.string.isRequired,
+};
+
+SocialLinkButton.defaultProps = {
+  href: null,
+  dark: false,
+  active: false,
+  unactive: false,
+  onClick: null,
+  withRemoveButton: false,
 };
