5a78741af877be4ef5ae715bfef70c213c47331c
Svetlana <sbatova@bitia.ru>
2019-09-24 17:41:34 +0300

    GKN-167: Add NoticeForm

--- src/RoutesShowModal/RoutesShowModal.js
+++ src/RoutesShowModal/RoutesShowModal.js
@@ -15,6 +15,10 @@ import { DEFAULT_COMMENTS_DISPLAYED } from '../Constants/Comments';
 import StickyBar from '../StickyBar/StickyBar';
 import SchemeModal from '../SchemeModal/SchemeModal';
 import ShowSchemeButton from '../ShowSchemeButton/ShowSchemeButton';
+import NoticeButton from '../NoticeButton/NoticeButton';
+import NoticeForm from '../NoticeForm/NoticeForm';
+import Tooltip from '../Tooltip/Tooltip';
+import { avail } from '../Utils';
 import TooltipPerson from '../TooltipPerson/TooltipPerson';
 import { HIDE_DELAY } from '../Constants/TooltipPerson';
 import numToStr from '../Constants/NumToStr';
@@ -24,7 +28,6 @@ export default class RoutesShowModal extends Component {
   constructor(props) {
     super(props);
 
-    const { route } = this.props;
     this.state = {
       quoteComment: null,
       commentContent: '',
@@ -33,6 +36,8 @@ export default class RoutesShowModal extends Component {
       currentPointers: [],
       routeImageLoading: true,
       schemeModalVisible: false,
+      showNoticeForm: false,
+      showTooltip: false,
       showLikesTooltip: false,
       showRedpointsTooltip: false,
       showFlashesTooltip: false,
@@ -153,6 +158,22 @@ export default class RoutesShowModal extends Component {
     );
   };
 
+  showNoticeForm = (event) => {
+    event.stopPropagation();
+    this.setState({ showTooltip: false, showNoticeForm: true });
+  };
+
+  cancelNoticeForm = (event) => {
+    event.stopPropagation();
+    this.setState({ showTooltip: false, showNoticeForm: false });
+  };
+
+  submitNoticeForm = (msg) => {
+    const { submitNoticeForm } = this.props;
+    submitNoticeForm(msg);
+    this.setState({ showTooltip: false, showNoticeForm: false });
+  };
+
   content = () => {
     const {
       onClose,
@@ -189,6 +210,8 @@ export default class RoutesShowModal extends Component {
       showLikesTooltip,
       showRedpointsTooltip,
       showFlashesTooltip,
+      showNoticeForm,
+      showTooltip,
     } = this.state;
     const showLoadPhotoMsg = (
       (!route.photo || !routeImageLoading) && user && this.canEditRoute(user, route)
@@ -205,6 +228,25 @@ export default class RoutesShowModal extends Component {
               }
             />
           </div>
+          {
+            (user && avail(user.id)) && <div
+              className="modal-block__notice"
+              onMouseEnter={() => this.setState({ showTooltip: true })}
+              onMouseLeave={() => this.setState({ showTooltip: false })}
+              onClick={event => event.stopPropagation()}
+            >
+              <NoticeButton onClick={this.showNoticeForm} />
+              <div className="modal-block__notice-tooltip">
+                {showTooltip && <Tooltip text="Сообщить об ошибке" />}
+              </div>
+              {
+                showNoticeForm && <NoticeForm
+                  submit={this.submitNoticeForm}
+                  cancel={this.cancelNoticeForm}
+                />
+              }
+            </div>
+          }
           {
             schemeModalVisible
               ? (
@@ -455,6 +497,267 @@ export default class RoutesShowModal extends Component {
     );
   };
 
+  showNoticeForm = (event) => {
+    event.stopPropagation();
+    this.setState({ showTooltip: false, showNoticeForm: true });
+  };
+
+  cancelNoticeForm = (event) => {
+    event.stopPropagation();
+    this.setState({ showTooltip: false, showNoticeForm: false });
+  };
+
+  submitNoticeForm = (msg) => {
+    const { submitNoticeForm } = this.props;
+    submitNoticeForm(msg);
+    this.setState({ showTooltip: false, showNoticeForm: false });
+  };
+
+  content = () => {
+    const {
+      onClose,
+      route,
+      numOfLikes,
+      isLiked,
+      likeBtnIsBusy,
+      onLikeChange,
+      user,
+      numOfRedpoints,
+      numOfFlash,
+      ctrlPressed,
+      removeRoute,
+      openEdit,
+      ascent,
+      changeAscentResult,
+      removeComment,
+      comments,
+      numOfComments,
+      goToProfile,
+      diagram,
+    } = this.props;
+    const {
+      currentPointers,
+      descriptionCollapsed,
+      numOfDisplayedComments,
+      quoteComment,
+      commentContent,
+      routeImageLoading,
+      schemeModalVisible,
+      showNoticeForm,
+      showTooltip,
+    } = this.state;
+    const showLoadPhotoMsg = (
+      (!route.photo || !routeImageLoading) && user && this.canEditRoute(user, route)
+    );
+    return (
+      <div className="modal-overlay__wrapper">
+        <div className="modal modal-overlay__modal">
+          <div className="modal-block__close">
+            <CloseButton
+              onClick={
+                schemeModalVisible
+                  ? () => this.setState({ schemeModalVisible: false })
+                  : () => onClose()
+              }
+            />
+          </div>
+          {
+            (user && avail(user.id)) && <div
+              className="modal-block__notice"
+              onMouseEnter={() => this.setState({ showTooltip: true })}
+              onMouseLeave={() => this.setState({ showTooltip: false })}
+              onClick={event => event.stopPropagation()}
+            >
+              <NoticeButton onClick={this.showNoticeForm} />
+              <div className="modal-block__notice-tooltip">
+                {showTooltip && <Tooltip text="Сообщить об ошибке" />}
+              </div>
+              {
+                showNoticeForm && <NoticeForm
+                  submit={this.submitNoticeForm}
+                  cancel={this.cancelNoticeForm}
+                />
+              }
+            </div>
+          }
+          {
+            schemeModalVisible
+              ? (
+                <SchemeModal
+                  currentRoute={route}
+                  diagram={diagram}
+                  save={() => this.setState({ schemeModalVisible: false })}
+                  close={() => this.setState({ schemeModalVisible: false })}
+                />
+              )
+              : (
+                <>
+                  <div
+                    className="modal__track-block"
+                    role="button"
+                    tabIndex={0}
+                    style={{ outline: 'none' }}
+                    onMouseOver={() => {
+                      this.mouseOver = true;
+                    }}
+                    onMouseLeave={() => {
+                      this.mouseOver = false;
+                    }}
+                  >
+                    <div className="modal__track">
+                      <ShowSchemeButton
+                        disabled={route.data.position === undefined}
+                        onClick={() => this.setState({ schemeModalVisible: true })}
+                      />
+                      {
+                        showLoadPhotoMsg && (
+                          <div className="modal__track-descr">
+                            <div className="modal__track-descr-picture" />
+                            <div className="modal__track-descr-text">Загрузите фото трассы</div>
+                          </div>
+                        )
+                      }
+                      {
+                        route.photo
+                          ? (
+                            <RouteEditor
+                              route={route}
+                              routePhoto={route.photo.url}
+                              pointers={currentPointers}
+                              editable={false}
+                              updatePointers={this.updatePointers}
+                              routeImageLoading={routeImageLoading}
+                              onImageLoad={() => this.setState({ routeImageLoading: false })}
+                            />
+                          )
+                          : ''
+                      }
+                    </div>
+                    <div
+                      className="modal__track-footer"
+                    >
+                      <div className="modal__track-information">
+                        <div className="modal__track-count">
+                          <LikeButton
+                            numOfLikes={numOfLikes}
+                            isLiked={isLiked}
+                            busy={likeBtnIsBusy}
+                            onChange={!user ? null : onLikeChange}
+                          />
+                        </div>
+                        <div className="modal__track-count">
+                          <Counter number={numOfRedpoints} text="redpoints" />
+                        </div>
+                        <div className="modal__track-count">
+                          <Counter number={numOfFlash} text="flash" />
+                        </div>
+                      </div>
+                      {
+                        (user && this.canEditRoute(user, route)) && (
+                          ctrlPressed
+                            ? (
+                              <Button
+                                size="small"
+                                style="normal"
+                                title="Удалить"
+                                onClick={removeRoute}
+                              />
+                            )
+                            : (
+                              <Button
+                                size="small"
+                                style="normal"
+                                title="Редактировать"
+                                onClick={openEdit}
+                              />
+                            )
+                        )
+                      }
+                    </div>
+                  </div>
+                  <div
+                    className="modal__track-info"
+                    onMouseOver={() => {
+                      this.mouseOver = true;
+                    }}
+                    onMouseLeave={() => {
+                      this.mouseOver = false;
+                    }}
+                  >
+                    <div className="modal__track-status">
+                      {
+                        user && (
+                          <RouteStatus
+                            ascent={ascent}
+                            changeAscentResult={changeAscentResult}
+                          />
+                        )
+                      }
+                    </div>
+                    <div className="modal__track-header">
+                      <h1 className="modal__title">
+                        {route.number ? `№ ${route.number}` : `# ${route.id}`}
+                        <span className="modal__title-place-wrapper">
+                          <span className="modal__title-place">
+                            {route.name ? `(“${route.name}”)` : ''}
+                          </span>
+                        </span>
+                      </h1>
+                      <RouteDataTable route={route} user={user} />
+                    </div>
+                    <div className="modal__item modal__descr-item">
+                      <CollapsableBlock
+                        title="Описание"
+                        isCollapsed={descriptionCollapsed}
+                        onCollapseChange={this.onDescriptionCollapseChange}
+                        text={route.description ? route.description : ''}
+                      />
+                    </div>
+                    <div className="modal__item">
+                      <CommentBlock
+                        startAnswer={this.startAnswer}
+                        user={user}
+                        removeComment={removeComment}
+                        allShown={
+                          comments.length === R.min(
+                            numOfDisplayedComments,
+                            comments.length,
+                          )
+                        }
+                        numOfComments={numOfComments}
+                        showPrevious={this.showPreviousComments}
+                        onCollapseChange={this.onDescriptionCollapseChange}
+                        comments={
+                          R.slice(
+                            comments.length - R.min(numOfDisplayedComments, comments.length),
+                            comments.length,
+                            comments,
+                          )
+                        }
+                        objectListTitle="route_comments"
+                      />
+                    </div>
+                    <div className="modal__enter-comment">
+                      <CommentForm
+                        quoteComment={quoteComment}
+                        setTextareaRef={this.setTextareaRef}
+                        goToProfile={goToProfile}
+                        user={user}
+                        content={commentContent}
+                        saveComment={this.saveComment}
+                        onContentChange={this.onCommentContentChange}
+                        removeQuoteComment={this.removeQuoteComment}
+                      />
+                    </div>
+                  </div>
+                </>
+              )
+          }
+        </div>
+      </div>
+    );
+  };
+
   render() {
     const {
       onClose, numOfActiveRequests,
@@ -505,6 +808,7 @@ RoutesShowModal.propTypes = {
   onLikeChange: PropTypes.func.isRequired,
   changeAscentResult: PropTypes.func.isRequired,
   numOfActiveRequests: PropTypes.number.isRequired,
+  submitNoticeForm: PropTypes.func.isRequired,
 };
 
 RoutesShowModal.defaultProps = {
