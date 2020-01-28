39cad5ffb994c23a2191c784c33f5db6346c323e
ALev <alev@bitia.ru>
2020-01-29 02:22:05 +0300

    GKN-141: Refactor loading indicator

---
+++ src/v2/components/LoadingIndicator/LoadingIndicator.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from '../../aphrodite';
+
+
+class LoadingIndicator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  startLoading = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  stopLoading = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    const { children, showAlways } = this.props;
+    const isLoading = this.state.counter > 0;
+
+    return (
+      <div className={css(style.container)} ref={() => { window.loadingIndicator = this; }}>
+        <div className={css(style.wrapper)}>
+          {children}
+        </div>
+
+        <div className={css(style.indicatorContainer)}>
+          <div
+            className={
+              css(
+                style.indicator,
+                isLoading ? style.indicatorActive : (!showAlways && style.hideIndicator),
+              )
+            }
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+    height: '100%',
+  },
+  wrapper: {
+    height: '100%',
+    marginBottom: '-3px',
+  },
+  indicatorContainer: {
+    position: 'sticky',
+    bottom: 0,
+    height: '3px',
+    width: '100%',
+    backgroundColor: 'transparent',
+    overflow: 'hidden',
+    zIndex: 999,
+  },
+  indicator: {
+    backgroundColor: '#006CEB',
+    height: '100%',
+    width: '100%',
+    transition: 'width 0.5s ease-out,opacity 0.5s linear',
+  },
+  indicatorActive: {
+    opacity: 1,
+    animationName: [{
+      '0%': {
+        width: 0,
+      },
+    }],
+    animationDuration: '20s',
+    animationTimingFunction: 'cubic-bezier(0, 0.78, 1, 0.53)',
+    animationDelay: '0s',
+    animationIterationCount: 'infinite',
+  },
+  hideIndicator: {
+    opacity: 0,
+  },
+});
+
+LoadingIndicator.propTypes = {
+  showAlways: PropTypes.bool,
+  isLoading: PropTypes.bool,
+};
+
+LoadingIndicator.defaultProps = {
+  showAlways: false,
+  isLoading: false,
+};
+
+export default LoadingIndicator;
