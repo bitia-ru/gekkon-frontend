e2e31c783f0dbd3f41e79896673c32246026e0a8
ALev <alev@bitia.ru>
2020-02-13 05:08:14 +0300

    Switch to user v2 components

--- src/v1/Main.js
+++ src/v1/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import SpotsIndex from './components/Spots/SpotsIndex';
-import SpotsShow from './components/Spots/SpotsShow';
 import CragsIndex from './components/Crags/CragsIndex';
 import About from './components/About/About';
 import Faq from './components/Faq/Faq';
@@ -18,15 +17,11 @@ const Main = ({ currentUserId }) => (
         )
         : (
           <Switch>
-            <Route exact path="/" component={SpotsIndex} />
+            <Route exact path={['/', '/spots']} component={SpotsIndex} />
             <Route exact path="/crags" component={CragsIndex} />
             <Route exact path="/about" component={About} />
             <Route exact path="/faq" component={Faq} />
-            <Route path="/v2" component={V2} />
-            <Route path="/spots/:id/sectors/:sector_id/routes" component={SpotsShow} />
-            <Route path="/spots/:id/sectors/:sector_id" component={SpotsShow} />
-            <Route path="/spots/:id/routes" component={SpotsShow} />
-            <Route path="/spots/:id" component={SpotsShow} />
+            <Route path={['/users', '/spots']} component={V2} />
           </Switch>
         )
     }
