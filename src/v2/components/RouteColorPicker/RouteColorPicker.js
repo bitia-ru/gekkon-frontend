705042e8653230121d5f2940b64ac27e5e5ee2e9
Vertigo <vertigomaill@gmail.com>
2020-02-16 20:02:05 +0300

    GKN-206: Refactoring RouteDataTable.js, RouteColorPicker.js

---
+++ src/v2/components/RouteColorPicker/RouteColorPicker.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import * as R from 'ramda';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { getColorStyle } from '../../Constants/Route';
+import { StyleSheet, css } from '../../aphrodite';
+
+export default class RouteColorPicker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      droppedDown: false,
+    };
+    this.mouseOver = false;
+  }
+
+    selectItem = (routeMarkColor) => {
+      const { onSelect } = this.props;
+      this.setState({ droppedDown: false });
+      onSelect(routeMarkColor);
+    };
+
+    onBlur = () => {
+      if (!this.mouseOver) {
+        this.setState({ droppedDown: false });
+      }
+    };
+
+    render() {
+      const {
+        route, fieldName, editable, routeMarkColors,
+      } = this.props;
+      const { droppedDown } = this.state;
+      const droppedDownClasses = classNames({
+        'combo-box__dropdown': true,
+        'modal__combo-box-drowdown': true,
+        'combo-box__dropdown_active': true,
+      });
+      const itemClasses = classNames({
+        'combo-box__dropdown-item': true,
+        'combo-box__dropdown-item_padding-10': true,
+      });
+      return (
+        <div
+          className={css(styles.markColorPickerWrap)}
+          role="button"
+          onClick={() => {
+            if (editable) {
+              this.setState({ droppedDown: !droppedDown });
+            }
+          }}
+          onBlur={this.onBlur}
+          tabIndex={1}
+          onMouseLeave={() => {
+            this.mouseOver = false;
+          }}
+          onMouseOver={() => {
+            this.mouseOver = true;
+          }}
+        >
+          <div className={css(styles.markColorPickerInfo)}>
+            <div
+              className={css(styles.markColorPickerColor)}
+              style={getColorStyle(route[fieldName])}
+            />
+          </div>
+          <div className={css(styles.markColorPickerName)}>
+            {
+              route[fieldName]
+                ? route[fieldName].name
+                : 'не задан'
+            }
+          </div>
+          {
+            droppedDown
+              ? (
+                <div
+                  className={droppedDownClasses}
+                >
+                  <div
+                    className="combo-box__dropdown-wrapper"
+                  >
+                    {R.map(routeMarkColor => (
+                      <li
+                        key={routeMarkColor.id}
+                        onClick={() => this.selectItem(routeMarkColor)}
+                        className={itemClasses}
+                      >
+                        <div className={css(styles.markColorPickerItem)}>
+                          <div
+                            className={css(styles.markColorPickerColor)}
+                            style={getColorStyle(routeMarkColor)}
+                          />
+                          <div className={css(styles.markColorPickerItemText)}>
+                            {routeMarkColor.name}
+                          </div>
+                        </div>
+                      </li>
+                    ), routeMarkColors)}
+                  </div>
+                </div>
+              )
+              : ''
+          }
+        </div>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  markColorPickerName: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    paddingLeft: '20px',
+    cursor: 'pointer',
+    whiteSpace: 'nowrap',
+  },
+  markColorPickerWrap: {
+    display: 'inline-flex',
+    position: 'relative',
+    outline: 'none',
+  },
+  markColorPickerInfo: {
+    cursor: 'pointer',
+  },
+  markColorPickerColor: {
+    display: 'inline-block',
+    width: '60px',
+    height: '20px',
+    verticalAlign: 'middle',
+    backgroundPosition: 'center',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+  },
+  markColorPickerItem: {
+    display: 'flex',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  markColorPickerItemText: {
+    fontSize: '16px',
+    fontFamily: ['GilroyRegular', 'sans-serif'],
+    paddingLeft: '20px',
+  },
+});
+
+RouteColorPicker.propTypes = {
+  onSelect: PropTypes.func,
+  routeMarkColors: PropTypes.array,
+  route: PropTypes.object.isRequired,
+  editable: PropTypes.bool.isRequired,
+  fieldName: PropTypes.string.isRequired,
+};
+
+RouteColorPicker.defaultProps = {
+  onSelect: null,
+  routeMarkColors: null,
+};
---
+++ src/v2/components/RouteDataTable/RouteDataTable.js
@@ -0,0 +1,190 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import * as R from 'ramda';
+import { getCategoryColor } from '../../Constants/Categories';
+import { GetUserName } from '../../Constants/User';
+import { ROUTE_KINDS } from '../../Constants/Route';
+import RouteColorPicker from '../RouteColorPicker/RouteColorPicker';
+import { StyleSheet, css } from '../../aphrodite';
+
+const RouteDataTable = ({
+  user, route,
+}) => {
+  const isCurrentUserRoute = user && route.author_id === user.id;
+  let name = route.author ? GetUserName(route.author) : null;
+  if (!isCurrentUserRoute && name === null && route.author_id !== null) {
+    if (user && user.role === 'admin') {
+      name = GetUserName(route.author, true);
+    }
+    if (user && user.role === 'creator') {
+      name = `Пользователь #${route.author.id}`;
+    }
+  }
+  const kind = R.find(R.propEq('title', route.kind), ROUTE_KINDS);
+  return (
+  // ITS NOT DEFINED
+    <div className="route-data-table">
+      {/* END*/}
+      <div className={css(styles.routeDataTableRow)}>
+        <div className={css(styles.routeDataTableItem, styles.routeDataTableItemHeader)}>
+          Категория:
+        </div>
+        <div className={css(styles.routeDataTableItem)}>
+          <div className={css(styles.routeDataTableCategoryTrack)}>{route.category}</div>
+          <div
+            className={css(styles.routeDataTableCategoryTrackColor)}
+            style={{ backgroundColor: getCategoryColor(route.category) }}
+          />
+        </div>
+      </div>
+      <div className={css(styles.routeDataTableRow)}>
+        <div className={css(styles.routeDataTableItem, styles.routeDataTableItemHeader)}>
+          Зацепы:
+        </div>
+        <div className={css(styles.routeDataTableItem)}>
+          <RouteColorPicker editable={false} route={route} fieldName="holds_color" />
+        </div>
+      </div>
+      {
+        route.marks_color && (
+          <div className={css(styles.routeDataTableRow)}>
+            <div className={css(styles.routeDataTableItem, styles.routeDataTableItemHeader)}>
+              Маркировка:
+            </div>
+            <div className={css(styles.routeDataTableItem)}>
+              <RouteColorPicker editable={false} route={route} fieldName="marks_color" />
+            </div>
+          </div>
+        )
+      }
+      <div className={css(styles.routeDataTableRow)}>
+        <div className={css(styles.routeDataTableItem, styles.routeDataTableItemHeader)}>
+          Тип:
+        </div>
+        <div className={css(styles.routeDataTableItem)}>
+          {kind && kind.text}
+        </div>
+      </div>
+      {
+        route.installed_at && (<>
+          <div className={css(styles.routeDataTableRow)}>
+            <div className={css(styles.routeDataTableItem, styles.routeDataTableItemHeader)}>
+              Накручена:
+            </div>
+            <div className={css(styles.routeDataTableItem)}>
+              {moment(route.installed_at).format('DD.MM.YYYY')}
+            </div>
+          </div>
+        </>)
+      }
+      {
+        route.installed_until && (<>
+          <div className={css(styles.routeDataTableRow)}>
+            <div className={css(styles.routeDataTableItem, styles.routeDataTableItemHeader)}>
+              Cкручена:
+            </div>
+            <div className={css(styles.routeDataTableItem)}>
+              {moment(route.installed_until).format('DD.MM.YYYY')}
+            </div>
+          </div>
+        </>)
+      }
+      {
+        name && (<>
+          <div className={css(styles.routeDataTableRow)}>
+            <div className={css(styles.routeDataTableItem, styles.routeDataTableItemHeader)}>
+              Накрутчик:
+            </div>
+            <div className={css(styles.routeDataTableItem)}>
+              <a className={css(styles.routeDataTableLink)}>{isCurrentUserRoute ? 'Вы' : name}</a>
+            </div>
+          </div>
+        </>)
+      }
+    </div>
+  );
+};
+
+const styles = StyleSheet.create({
+  routeDataTableRow: {
+    display: 'flex',
+    padding: '5px 0 5px',
+    '@media screen and (max-width: 1440px)': {
+      padding: '3px 0 3px',
+    },
+  },
+  routeDataTableItem: {
+    maxWidth: '215px',
+    width: '100%',
+    fontSize: '16px',
+    '@media screen and (max-width: 1440px)': {
+      fontSize: '14px',
+    },
+  },
+  routeDataTableItemHeader: {
+    color: '#828282',
+    '@media screen and (max-width: 1440px)': {
+      minWidth: '150px',
+    },
+  },
+
+  // NOT USED
+  routeDataTableTableItemRight: {
+    maxWidth: '240px',
+    width: ' 100%',
+  },
+  routedataTableFieldSelect: {
+    marginLeft: '-10px',
+    marginTop: '-3px',
+  },
+  // END
+
+  routeDataTableCategoryTrack: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+  },
+
+  // NOT USED
+  routeDataTableCategoryTrackWrap: {
+    display: 'inline-flex',
+    position: 'relative',
+  },
+  routeDataTableCategoryTrackInfo: {
+    cursor: 'pointer',
+  },
+  // END
+
+  routeDataTableCategoryTrackColor: {
+    display: 'inline-block',
+    width: '60px',
+    height: '20px',
+    verticalAlign: 'middle',
+    marginLeft: '15px',
+  },
+
+  // NOT USED
+  routeDataTableCategoryTrackColor5c: {
+    backgroundColor: '#E24D4D',
+  },
+  // END
+
+  routeDataTableLink: {
+    color: '#006CEB',
+    textDecoration: 'none',
+    cursor: 'pointer',
+    '@media screen and (max-width: 1440px)': {
+      whiteSpace: 'nowrap',
+    },
+    ':hover': {
+      textDecoration: 'underline',
+    },
+  },
+});
+
+RouteDataTable.propTypes = {
+  user: PropTypes.object,
+  route: PropTypes.object.isRequired,
+};
+
+export default RouteDataTable;
