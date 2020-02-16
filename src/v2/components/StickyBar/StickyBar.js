614abaf5be30e6759db1f49bef3360abc82a4908
Vertigo <vertigomaill@gmail.com>
2020-02-16 22:47:11 +0300

    GKN-206: Refactoring StickyBar.js

---
+++ src/v2/components/StickyBar/StickyBar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from '../../aphrodite';
+
+const StickyBar = ({
+  children, loading, hideLoaded,
+}) => {
+  return (
+    <div className="sticky-bar">
+      <div className="wrapper">
+        {children}
+      </div>
+      <div className={css(styles.stickyBarItem)}>
+        <div
+          style={(loading ? {} : (hideLoaded ? { opacity: 0 } : {}))}
+          className={css(styles.stickyBarItemIndicator, loading ? styles.stickyBarItemIndicatorActive : '')}
+        />
+      </div>
+    </div>
+  );
+};
+
+const styles = StyleSheet.create({
+  stickyBarItem: {
+    position: 'sticky',
+    bottom: '0',
+    height: '3px',
+    width: '100%',
+    backgroundColor: 'transparent',
+    overflow: 'hidden',
+    zIndex: '2',
+  },
+  // ??
+  '@keyframes loading': {
+    '0%': {
+      width: '0',
+    },
+  },
+  // END
+  stickyBarItemIndicator: {
+    backgroundColor: '#006CEB',
+    height: '100%',
+    width: '100%',
+    transition: 'width 0.5s ease-out,opacity 0.5s linear',
+  },
+  stickyBarItemIndicatorActive: {
+    animation: 'loading 20s cubic-bezier(0, 0.78, 1, 0.53) 0s infinite',
+    opacity: '1',
+  },
+});
+
+StickyBar.propTypes = {
+  hideLoaded: PropTypes.bool,
+  loading: PropTypes.bool,
+};
+
+StickyBar.defaultProps = {
+  hideLoaded: false,
+  loading: true,
+};
+
+export default StickyBar;
