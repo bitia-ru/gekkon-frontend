d9704ceefe74afc9e48c8c43aebe19157c770079
Vertigo <vertigomaill@gmail.com>
2020-02-28 09:44:54 +0300

    GKN-214: Refactoring Faq.jsx (remove import InfoPageContent.js)

--- src/v2/components/Faq/Faq.jsx
+++ src/v2/components/Faq/Faq.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-// import { ToastContainer } from 'react-toastr';
-// import Cookies from 'js-cookie';
-// import InfoPageHeader from '@/v1/components/InfoPageHeader/InfoPageHeader';
-import InfoPageContent from '@/v1/components/InfoPageContent/InfoPageContent';
+// import InfoPageContent from '@/v1/components/InfoPageContent/InfoPageContent';
+import PropTypes from 'prop-types';
+import * as R from 'ramda';
 import { TITLE, TITLES, FAQ_DATA } from '@/v1/Constants/Faq';
-import getState from '@/v1/utils/getState';
 import MainScreen from '../../layouts/MainScreen/MainScreen';
 import Background from './images/faq.jpg';
 import { StyleSheet, css } from '../../aphrodite';
 
 class Faq extends React.PureComponent {
   render() {
+    const mapIndexed = R.addIndex(R.map);
     return (
       <MainScreen header={TITLE} styles={[styles.aboutUsHeader, styles.aboutUsHeaderTitle]}>
-        <InfoPageContent titles={TITLES} data={FAQ_DATA} />
+        <div className={css(styles.aboutUsContent)}>
+          <div className={css(styles.aboutUsContentContainer)}>
+            {
+              mapIndexed(
+                (paragraphs, index) => (
+                  <React.Fragment key={index}>
+                    <h2 className={css(styles.aboutUsContentTitle)}>{TITLES[index]}</h2>
+                    {
+                      mapIndexed(
+                        (paragraph, i) => (
+                          <p key={i} className={css(styles.aboutUsContentText)}>
+                            {paragraph}
+                          </p>
+                        ),
+                        paragraphs,
+                      )
+                        }
+                  </React.Fragment>
+                ),
+                FAQ_DATA,
+              )
+            }
+          </div>
+        </div>
       </MainScreen>
     );
   }
@@ -43,10 +65,39 @@ const styles = StyleSheet.create({
       marginBottom: '60px',
     },
   },
+  aboutUsContent: {
+    paddingTop: '75px',
+    paddingBottom: '75px',
+  },
+  aboutUsContentContainer: {
+    width: '100%',
+    maxWidth: '700px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  aboutUsContentTitle: {
+    fontSize: '34px',
+    marginBottom: '40px',
+    marginTop: '0',
+    fontFamily: ['GilroyBold', 'sans-serif'],
+    maxWidth: '380px',
+    lineHeight: '42px',
+    color: '#1F1F1F',
+  },
+  aboutUsContentText: {
+    fontSize: '18px',
+    lineHeight: '32px',
+    marginBottom: '28px',
+    marginTop: '28px',
+    color: '#1f1f1f',
+  },
 });
 
-const mapStateToProps = state => ({
+Faq.propTypes = {
+  titles: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
+};
 
-});
+const mapStateToProps = state => ({});
 
 export default withRouter(connect(mapStateToProps)(Faq));
