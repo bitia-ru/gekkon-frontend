c157fab9d5b7890974bbf39d79395032967e68b5
ALev <alev@bitia.ru>
2020-03-05 00:38:54 +0300

    GKN-218: Fix problems with save button on SchemeModal

--- src/v1/components/SchemeModal/SchemeModal.js
+++ src/v1/components/SchemeModal/SchemeModal.js
@@ -136,7 +136,7 @@ class SchemeModal extends Component {
         <div
           role="button"
           tabIndex={0}
-          style={{ outline: 'none' }}
+          style={{ margin: '50px 70px', outline: 'none' }}
           className="modal__hall-scheme"
           ref={(ref) => {
             this.schemeContainerRef = ref;
