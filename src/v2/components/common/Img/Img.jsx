0c22bb9d869b4e43becfcb7ae7824baff7e31551
Vertigo <vertigomaill@gmail.com>
2020-03-19 00:56:56 +0300

    GKN-217: Rework Img component

--- src/v2/components/common/Img/Img.jsx
+++ src/v2/components/common/Img/Img.jsx
@@ -2,9 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import defaultAvatar from './images/avatar_placeholder.svg';
 
-const Img = ({ height, src }) => (
-  <img height={height} src={(src || defaultAvatar)} />
-);
+class Img extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      image: undefined,
+    };
+
+    this.imagesInternal = {};
+  }
+
+  componentDidMount() {
+    this.processProps(this.props);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      this.processProps(this.props);
+    }
+  }
+
+  processProps(props) {
+    this.imagesInternal[props.src] = new Image();
+    this.imagesInternal[props.src].src = props.src;
+    this.setState(
+      { image: undefined },
+      () => {
+        this.imagesInternal[props.src].onload = () => {
+          this.setState({ image: props.src });
+        };
+      },
+    );
+  }
+
+  render() {
+    const { style, placeholder } = this.props;
+    const { image } = this.state;
+    return (
+      <img height={style.height} src={image ? `${image}` : placeholder} />
+    );
+  }
+}
 
 Img.PropTypes = {
   height: PropTypes.number,
