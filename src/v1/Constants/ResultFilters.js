193f7937bcee37b97012fa397a62d43b2451a87b
Svetlana Batova <sbatova@bitia.ru>
2020-03-21 18:05:32 +0300

    GKN-235: Remove filter duplicates

--- src/v1/Constants/ResultFilters.js
+++ src/v1/Constants/ResultFilters.js
@@ -1,11 +1,5 @@
-export const RESULT_FILTERS = [
-  {
-    id: 'unsuccessful', text: 'Не пройдена', clickable: true, value: 'unsuccessful',
-  },
-  {
-    id: 'red_point', text: 'Пролез', clickable: true, value: 'red_point',
-  },
-  {
-    id: 'flash', text: 'Флешанул', clickable: true, value: 'flash',
-  },
-];
+export const RESULT_FILTERS = {
+  unsuccessful: true,
+  red_point: true,
+  flash: true,
+};
