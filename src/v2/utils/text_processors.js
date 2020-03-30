726c3066bc09f6757727e3d11221eedca28b08fe
Vertigo <vertigomaill@gmail.com>
2020-03-30 09:07:33 +0300

    GKN-234: Rework wrapWebLinksInText function

--- src/v2/utils/text_processors.js
+++ src/v2/utils/text_processors.js
@@ -2,13 +2,12 @@ import React from 'react';
 import * as R from 'ramda';
 
 export const wrapWebLinksInText = (text) => {
-  const regExp = /\b(?:([A-Za-z]+):\/\/)?(?:([A-Za-z0-9]+)\:([A-Za-z0-9]+)\@)?((?:[A-Za-z0-9]+\.)+[A-Za-z0-9]+)(?:\:([0-9]+))?(?:\/((?:[A-Za-z0-9]+)(?:\/[A-Za-z0-9]+)*(?:\.[A-Za-z0-9]+)?)?(\?[A-Za-z0-9]+=[A-Za-z0-9]+(?:&[A-Za-z0-9]+=[A-Za-z0-9]+)*)?(?:#([A-Za-z0-9]+))?)?\s/gi;
-  const linkExtractor = '_^link^_';
-  const preparedText = text.replace(regExp, s => `${linkExtractor}${s}${linkExtractor}`);
+  const regExp = /\b((?:[A-Za-z]+:\/\/)?(?:[A-Za-z0-9]+\:[A-Za-z0-9]+\@)?(?:[A-Za-z0-9]+\.)+[A-Za-z0-9]+(?:\:[0-9]+)?(?:\/(?:[A-Za-z0-9]+(?:\/[A-Za-z0-9]+)*(?:\.[A-Za-z0-9]+)?)?(?:\?[A-Za-z0-9]+=[A-Za-z0-9]+(?:&[A-Za-z0-9]+=[A-Za-z0-9]+)*)?(?:#[A-Za-z0-9]+)?)?)\b/gi;
+  const preparedText = R.split(regExp, text);
   const mapIndexed = R.addIndex(R.map);
   return mapIndexed(
     (e, i) => (
       i % 2 === 0 ? e : <a key={`link${i}`} href={e}>{e}</a>
     ),
-  )(preparedText.split(linkExtractor));
+  )(preparedText);
 };
