c9b012735c3f585c9f587f0887963b9dba0876ae
ALev <alev@bitia.ru>
2020-07-17 17:47:16 +0300

    GKN-254: Update Vk integration callback

--- src/v1/Constants/Vk.js
+++ src/v1/Constants/Vk.js
@@ -2,4 +2,6 @@ import { ApiUrl } from '../Environ';
 import ClientId from '../ClientId';
 
 export const CLIENT_ID = ClientId;
-export const REDIRECT_URI = `https://rovingclimbers.ru${ApiUrl}/v1/integrations/vk/callbacks/oauth2`;
+export const REDIRECT_URI = (
+  `https://rovingclimbers.ru${ApiUrl}/v1/integrations/vk/callbacks/create`
+);
