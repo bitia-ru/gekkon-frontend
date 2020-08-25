a076673d9e93b4a672a73d8fb8ec2fac0bc5cab8
ALev <alev@bitia.ru>
2020-08-26 02:39:49 +0300

    GF-171: Move Counter styles into separate file

--- src/v2/components/Counter/Counter.js
+++ src/v2/components/Counter/Counter.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, css } from '../../aphrodite';
+import { css } from '../../aphrodite';
+import styles from './styles';
 
-const styles = StyleSheet.create({
-  counter: {
-    fontSize: '16px',
-    fontFamily: ['GilroyRegular', 'sans-serif'],
-    display: 'flex',
-    alignItems: 'center',
-    height: '100%',
-    '@media screen and (max-width: 1440px)': { fontSize: '14px' },
-  },
-  counterText: {
-    height: '100%',
-    textAlign: 'center',
-    lineHeight: '28px',
-  },
-  counterNumContainer: { marginRight: '4px' },
-  counterNumWrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#FFFFFF',
-    height: '100%',
-    width: '28px',
-    borderRadius: '50%',
-  },
-  counterNum: { marginTop: '2px' },
-  defaultCounterColor: { color: '#828282' },
-  redpointsTextColor: { color: '#E24D4D' },
-  flashesTextColor: { color: '#000000' },
-  redpointsNumColor: { backgroundColor: '#E24D4D' },
-  flashesNumColor: { backgroundColor: '#000000' },
-});
 
 const Counter = ({ number, text, type }) => (
   <div className={css(styles.counter)}>
