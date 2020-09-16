cb80bd9f7988de694e9162552227ffba8550bd4e
Svetlana Batova <sbatova@bitia.ru>
2020-09-16 16:08:06 +0300

    GF-193: Fix action type name bug and add infoData to spots storage

--- src/v2/redux/spots/actions.js
+++ src/v2/redux/spots/actions.js
@@ -1,5 +1,8 @@
 import Api from '../../utils/Api';
 import toastHttpError from '@/v2/utils/toastHttpError';
+import numToStr from '@/v1/Constants/NumToStr';
+import * as R from 'ramda';
+import { currentUser } from '../user_session/utils';
 
 export const acts = {
   LOAD_SPECIFIC_SPOT_REQUEST: 'LOAD_SPECIFIC_SPOT_REQUEST_V2',
@@ -10,19 +13,41 @@ export const acts = {
 };
 
 export const loadSpot = spotId => (
-  (dispatch) => {
+  (dispatch, getState) => {
     dispatch({
-      type: acts.LOAD_SPECIFIC_USER_REQUEST,
+      type: acts.LOAD_SPECIFIC_SPOT_REQUEST,
       spotId,
     });
 
     Api.get(
       `/v1/spots/${spotId}`,
       {
-        success(payload) {
+        success(payload, metadata) {
+          const spot = payload;
+          let infoData = [
+            {
+              count: metadata.num_of_sectors,
+              label: numToStr(metadata.num_of_sectors, ['Зал', 'Зала', 'Залов']),
+            },
+            {
+              count: metadata.num_of_routes,
+              label: numToStr(metadata.num_of_routes, ['Трасса', 'Трассы', 'Трасс']),
+            },
+          ];
+          const state = getState();
+          if (currentUser(state)) {
+            infoData = R.append({
+              count: metadata.num_of_unfulfilled,
+              label: numToStr(
+                metadata.num_of_unfulfilled,
+                ['Невыполненная трасса', 'Невыполненные трассы', 'Невыполненных трасс'],
+              ),
+            }, infoData);
+          }
+          spot.infoData = infoData;
           dispatch({
             type: acts.LOAD_SPOTS_SUCCESS,
-            spot: payload,
+            spot,
           });
         },
         failed(error) {
