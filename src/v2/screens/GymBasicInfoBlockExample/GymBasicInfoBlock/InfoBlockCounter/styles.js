329a7cd816c720ae943635f222560fb1b1dab861
Svetlana Batova <sbatova@bitia.ru>
2020-09-16 16:17:45 +0300

    GF-193: Introduce InfoBlockCounter

---
+++ src/v2/screens/GymBasicInfoBlockExample/GymBasicInfoBlock/InfoBlockCounter/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from '@/v2/aphrodite';
+
+const styles = StyleSheet.create({
+  count: {
+    fontWeight: 'bold',
+    fontFamily: 'GilroyBold',
+    fontSize: '36px',
+    lineHeight: '45px',
+  },
+  label: {
+    fontSize: '18px',
+    lineHeight: '21px',
+    marginLeft: 10,
+  },
+});
+
+export default styles;
