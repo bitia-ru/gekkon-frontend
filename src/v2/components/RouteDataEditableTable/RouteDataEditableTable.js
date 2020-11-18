10dac02808225e66f6be5a74bfd250b3f797f6f5
Svetlana Batova <sbatova@bitia.ru>
2020-11-18 13:46:58 +0300

    GF-265: Fix auto category select

--- src/v2/components/RouteDataEditableTable/RouteDataEditableTable.js
+++ src/v2/components/RouteDataEditableTable/RouteDataEditableTable.js
@@ -29,8 +29,14 @@ class RouteDataEditableTable extends Component {
     };
 
     onMarksColorSelect = (marksColor) => {
-      const { onRouteParamChange } = this.props;
+      const { onRouteParamChange, spots, match } = this.props;
+      const spotId = match.params.id;
+      const spot = spots[spotId];
+      const { markColorToCategory } = spot.data;
       onRouteParamChange(marksColor, 'marks_color');
+      if (markColorToCategory && markColorToCategory[marksColor.id]) {
+        onRouteParamChange(markColorToCategory[marksColor.id], 'category');
+      }
     };
 
     render() {
@@ -258,6 +264,8 @@ class RouteDataEditableTable extends Component {
 
 RouteDataEditableTable.propTypes = {
   sectors: PropTypes.object.isRequired,
+  spots: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired,
   users: PropTypes.array.isRequired,
   onRouteParamChange: PropTypes.func.isRequired,
   routeMarkColors: PropTypes.array.isRequired,
@@ -265,6 +273,7 @@ RouteDataEditableTable.propTypes = {
 
 const mapStateToProps = state => ({
   sectors: state.sectorsStore.sectors,
+  spots: state.spotsStore.spots,
 });
 
 export default withRouter(connect(mapStateToProps)(RouteDataEditableTable));
